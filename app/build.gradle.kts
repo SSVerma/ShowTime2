4536c2d0bd4becb842956bc15d981ffd2bc5a2ad
SSVerma <36238594+SSVerma@users.noreply.github.com>
2026-05-30 13:49:41 +0530

    updates version code and name (#14)
    Co-authored-by: Shyam Sunder <ss@Shyams-MacBook-Air.local>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.ssverma.showtime"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 12
-        versionName = "1.0.8"
+        versionCode = 13
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
