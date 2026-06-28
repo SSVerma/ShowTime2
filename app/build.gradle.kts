f493f9cdc5ee815ad9f3f920faac58a24104449d
Shyam Sunder <ss@Shyams-MacBook-Air.local>
2026-06-28 17:27:08 +0530

    updates version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.ssverma.showtime"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "1.0.9"
+        versionCode = 14
+        versionName = "1.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
